a9b95d60dd95baa1e4c201fedf1fe859e79b9bb2
ricar8o <andres.martinez-d@mail.escuelaing.edu.co>
2020-02-27 17:31:48 -0500

    Adelanto Front

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,14 +1,22 @@
-var app= (function () {
-  var author;
-  var blueprints;
 
-  var authorsBluePrint = function(author) {
-    alert(llllll)
-    $("table tbody").append(
-    apimock.getBlueprintsByAuthor(author).map(function(blueprint){
+var api = apimock;
+
+var app = (function () {
+  var author;
+  var blueprints = [];
+
+  var getAuthors = function(author){
+    api.getBlueprintsByAuthor(author, table);
+  }
+
+  var table = function(a, blueprintss) {
+    alert(blueprintss);
+    $("#blups").append(
+    blueprintss.map(function(blueprint){
       return "<tr><td>" + blueprint.name + "</td><td>" + blueprint.points.lenght + "</td><td><input type='checkbox' name='open'></td></tr>"
       })  
     );
+    alert("despues");
     var i = 0;
     apimock.getBlueprintsByAuthor(author).map(function(bluep){
       i+= bluep.points.lenght
@@ -17,6 +25,6 @@ var app= (function () {
   }
 
   return{
-    getBlueprints : authorsBluePrint
+      get : getAuthors
   };
 })();
