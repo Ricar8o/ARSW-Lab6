13907ad9a1e979a1bb63a6a65de0e254bf1fee0f
JulianGutierritos <julian.gutierrez-v@mail.escuelaing.edu.co>
2020-02-27 23:41:55 -0500

    Parte 1 terminada

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -6,21 +6,18 @@ var app = (function () {
   var blueprints = [];
 
   var getAuthors = function(author){
+	$("#blups").find("tr:gt(0)").remove();
     api.getBlueprintsByAuthor(author, table);
   }
 
   var table = function(a, blueprintss) {
-    alert(blueprintss);
+	var i = 0;
     $("#blups").append(
     blueprintss.map(function(blueprint){
-      return "<tr><td>" + blueprint.name + "</td><td>" + blueprint.points.lenght + "</td><td><input type='checkbox' name='open'></td></tr>"
+	  i+= blueprint.points.length;
+      return "<tr><td>" + blueprint.name + "</td><td>" + blueprint.points.length + "</td><td><input type='checkbox' name='open'></td></tr>"
       })  
     );
-    alert("despues");
-    var i = 0;
-    apimock.getBlueprintsByAuthor(author).map(function(bluep){
-      i+= bluep.points.lenght
-    })
     $("#total").text(i);
   }
 
